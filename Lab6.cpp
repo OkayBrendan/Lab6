e77e9f492b96c487abda9d3a9c1a3ea3277cb55b
OkayBrendan <washnakbrendan@gmail.com>
2023-10-30 10:45:56 -0400

    Add project files.

---
+++ Lab6.cpp
@@ -0,0 +1,57 @@
+// Lab6.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+using namespace std;
+
+int main() {
+	string background = "images1/backgrounds/prague.png";
+	string foreground = "images1/characters/yoda.png";
+
+	Texture backgroundTex;
+	if (!backgroundTex.loadFromFile(background)) {
+		cout << "Couldn't Load Image" << endl;
+		exit(1);
+	}
+
+	Texture foregroundTex;
+	if (!foregroundTex.loadFromFile(foreground)) {
+		cout << "Couldn't Load Image" << endl;
+		exit(1);
+	}
+
+	Image backgroundImage;
+	backgroundImage = backgroundTex.copyToImage();
+	Image foregroundImage;
+	foregroundImage = foregroundTex.copyToImage();
+
+	Vector2u sz = backgroundImage.getSize();
+	for (int y = 0; y < sz.y; y++) {
+		for (int x = 0; x < sz.x; x++) {
+			// These two loops will run the code inside for each pixel in the background image
+				// You can access the current pixel at x,y like so:
+			Color foregroundPixel = foregroundImage.getPixel(x, y);
+
+			// Check if the pixel is green
+			if (foregroundPixel.g > 200 && foregroundPixel.r < 100 && foregroundPixel.b < 100) {
+				// Replace the pixel with the corresponding pixel from the background
+				Color backgroundPixel = backgroundImage.getPixel(x, y);
+				foregroundImage.setPixel(x, y, backgroundPixel);
+			}
+		}
+	}
+
+	// By default, just show the foreground image
+	RenderWindow window(VideoMode(1024, 768), "Here's the output");
+	Sprite sprite1;
+	Texture tex1;
+	tex1.loadFromImage(foregroundImage);
+	sprite1.setTexture(tex1);
+	window.clear();
+	window.draw(sprite1);
+	window.display();
+	while (true);
+}
